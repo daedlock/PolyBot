cb10625eaea2b769e72609851ae1b1ab6166bbaf
Hossam Saraya <hossam.saraya@gmail.com>
2021-07-12 18:52:02 +0200

    less help

--- commands/help.js
+++ commands/help.js
@@ -27,15 +27,9 @@ const helpMessage = `
     !eth                fetch ETH price from pair reserve pool (realtime)
     !matic              fetch MATIC price from pair reserve pool (realtime)
     !qi                 fetch Qi price from pair reserve pool (realtime)
-    !wex                fetch WexPoly price from pair reserve pool (realtime)
-    !wexp               fetch WexPoly price from pair reserve pool (realtime)
-    !wexpoly            fetch WexPoly price from pair reserve pool (realtime)
     !addy               fetch ADDY price from pair reserve pool (realtime)
-    !pup                fetch PUP price from pair reserve pool (realtime)
-    !bone               fetch BONE price from pair reserve pool (realtime)
     !bunny              fetch POLYBUNNY price from pair reserve pool (realtime)
     !titan              fetch TITAN price from pair reserve pool (realtime)
-    !ecr                fetch collateral ratio (ECR & TCR)
     !alert              alert when a coin price goes off the threshold
     !alertstats         prints information about saved alerts
     !supportedcoins     a list of the supported coins
