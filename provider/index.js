d3cb23aef06d77584d7137c6ed288991466739eb
Hossam Saraya <hossam.saraya@gmail.com>
2021-10-10 15:45:37 +0200

    rest

--- provider/index.js
+++ provider/index.js
@@ -1,8 +1,10 @@
 const { ethers } = require('ethers')
 const matic = new ethers.providers.JsonRpcProvider(process.env.MATIC_RPC)
 const movr = new ethers.providers.JsonRpcProvider(process.env.MOVR_RPC)
+const eth = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC)
 
 module.exports = {
     matic,
     movr,
+    eth
 }
